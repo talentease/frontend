01109d4cd4c3ad71454dc5b5e77ec008645300e9
razanfawwaz <razanfawwaz1905@gmail.com>
2023-06-12 23:51:31 +0700

    feat: add register

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import { JetBrains_Mono as FontMono, Public_Sans as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
