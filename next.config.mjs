568739cf46c8e04d7ecaaaa03e23680e8261a567
razanfawwaz <razanfawwaz1905@gmail.com>
2023-06-13 00:29:56 +0700

    feat: firebase deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
+  trailingSlash: true
 }
 
 export default nextConfig
